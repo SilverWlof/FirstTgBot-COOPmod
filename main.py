8cfc20761d92dd05b8df44a1fab621972fb923da
SilverWlof <wlof55Gmail.com>
2024-03-10 02:51:34 +0400

    working_base

---
+++ main.py
@@ -0,0 +1,9 @@
+import telebot;
+
+bot = telebot.TeleBot('6456136259:AAElOCKMQPdWSveu67w_4vdOiq1j5dIcgnA')
+
+@bot.message_handler(commands=['start']) #отслеживание определенной команды
+def main(massage):  #def это функция
+  bot.send_message(massage.chat.id, 'Hellow')
+
+bot.polling(non_stop=True)
