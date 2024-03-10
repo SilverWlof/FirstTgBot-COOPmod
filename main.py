5e23d344ff2d8c4de1445004381471c28ba175db
SilverWlof <wlof55Gmail.com>
2024-03-10 12:58:56 +0400

    first

--- main.py
+++ main.py
@@ -6,8 +6,18 @@ bot = telebot.TeleBot('6456136259:AAElOCKMQPdWSveu67w_4vdOiq1j5dIcgnA')
 def main(massage):  #def это функция
   bot.send_message(massage.chat.id, 'Hellow') #параметра parase_mode='html' позволит писать в сообщение HTML
 
-@bot.message_handler(commands=['info']) #отслеживание определенной команды
+@bot.message_handler(commands=['info'])
 def info(massage):
   bot.send_message(massage.chat.id, massage) #вся инфа о чате и пользователи в чавте
+
+@bot.message_handler(commands=['faq']) 
+def info(massage):
+  bot.send_message(massage.chat.id, 'инфа о проекте',parse_mode="html") 
+
+@bot.message_handler()
+def info(message):
+  if message.text.lower() =="старт":
+      bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}') #параметра parase_mode='html' позволит писать в сообщение HTML
+
 
 bot.polling(non_stop=True) #не прерывность программы
